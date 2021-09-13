4d5cabd36d6c140dacba8c5e835a640057c51c46
Box Chen <box621863@gmail.com>
2021-09-14 05:43:28 +0800

    fix test3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        This is a test for Amplify
+        This is a test for Amplify 2
           
         </p>
         <a
